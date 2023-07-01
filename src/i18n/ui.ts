4859cd49f8d11f6f9f01644e5dbf8e742612b46d
Carlo Sebastian La Porta <carloalaple@gmail.com>
2023-07-01 15:28:32 +0200

    initial version 0.1

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: 'English',
+  es: 'Spanish',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'nav.home': 'Accueil',
+    'nav.about': 'À propos',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  },
+} as const;
